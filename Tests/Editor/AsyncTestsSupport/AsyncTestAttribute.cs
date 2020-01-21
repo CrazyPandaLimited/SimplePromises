14e27e5d8339b9dc0bc2f4e565fe4c7678b0d33e
druhasu <a.sudarikov@crazypanda.ru>
2020-01-21 12:37:29 +0300

    Improved support for UnityWebRequest in async tests Added specialized AsyncTestCase and AsyncTestCaseSource attributes Added helper method PandaTasksUtilitys.CallbackTask

--- Tests/Editor/AsyncTestsSupport/AsyncTestAttribute.cs
+++ Tests/Editor/AsyncTestsSupport/AsyncTestAttribute.cs
@@ -1,9 +1,12 @@
+﻿using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using NUnit.Framework.Internal;
 using NUnit.Framework.Internal.Builders;
 using NUnit.Framework.Internal.Commands;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using UnityEngine.TestTools;
 
 namespace CrazyPanda.UnityCore.PandaTasks
 {
@@ -15,8 +18,8 @@ namespace CrazyPanda.UnityCore.PandaTasks
     /// This may be usefull for occasions of very long requests, not called callbacks, etc.
     /// This way the whole test run will not be stuck waiting for a single test
     /// </summary>
-    [ AttributeUsage( AttributeTargets.Method, AllowMultiple = false ) ]
-    public class AsyncTestAttribute : Attribute, ISimpleTestBuilder, IWrapTestMethod, IImplyFixture
+    [AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+    public class AsyncTestAttribute : Attribute, ISimpleTestBuilder, IWrapSetUpTearDown, IImplyFixture, IApplyToTest
     {
         /// <summary>
         /// Test timeout.
@@ -44,25 +47,53 @@ namespace CrazyPanda.UnityCore.PandaTasks
 
         TestMethod ISimpleTestBuilder.BuildFrom( IMethodInfo method, Test suite )
         {
+            var wrapperMethod = new EnumeratorTaskWrapper( method, Timeout );
+
             // Set HasExpectedResult=true so call to BuildTestMethod will succeed            
             var parms = new TestCaseParameters() { HasExpectedResult = true };
-            var ret = new NUnitTestCaseBuilder().BuildTestMethod( method, suite, parms );
+            var ret = new NUnitTestCaseBuilder().BuildTestMethod( wrapperMethod, suite, parms );
 
             // Set HasExpectedResult=false so it won't fail later because expected result is not same as actual
             ret.parms.HasExpectedResult = false;
 
+            // check return type
             if( !typeof( IPandaTask ).IsAssignableFrom( method.ReturnType.Type ) && !typeof( Task ).IsAssignableFrom( method.ReturnType.Type ) )
             {
                 ret.RunState = RunState.NotRunnable;
                 ret.Properties.Set( "_SKIPREASON", "Method marked with AsyncTest must return either IPandaTask or System.Threading.Task" );
             }
-
+            
             return ret;
         }
 
         TestCommand ICommandWrapper.Wrap( TestCommand command )
         {
-            return new AsyncTestCommand( command.Test, Timeout );
+            return new UnityTestAttribute().Wrap( command );
+        }
+
+        void IApplyToTest.ApplyToTest( Test test )
+        {
+            // check if test cases are built using supported attributes
+            if( HasAttribute< TestCaseAttribute >( test ) )
+            {
+                test.RunState = RunState.NotRunnable;
+                test.Properties.Set( "_SKIPREASON", $"Replace TestCase's with AsyncTestCase's, because they are not supported on async tests" );
+            }
+            else if( HasAttribute< TestCaseSourceAttribute >( test ) )
+            {
+                test.RunState = RunState.NotRunnable;
+                test.Properties.Set( "_SKIPREASON", $"Replace TestCaseSource's with AsyncTestCaseSource's, because they are not supported on async tests" );
+            }
+        }
+
+        private bool HasAttribute< T >( Test test )
+            where T : Attribute
+        {
+            var attrs = test.Method.GetCustomAttributes< T >( true );
+            if( attrs.Length == 0 )
+                return false;
+
+            return attrs.Any( a => a.GetType() == typeof( T ) );
         }
     }
 }
