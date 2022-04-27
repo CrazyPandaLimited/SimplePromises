472d6d459966e7fa12e1183592cf93a0cab24809
Evgeniy Mineev <e.mineev@crazypanda.ru>
2022-04-27 20:26:06 +0400

    remove extra code from asyncbuilder and add obsolete attribute to it

--- Runtime/PandaTasks/IPandaTaskMethodBuilder.cs
+++ Runtime/PandaTasks/IPandaTaskMethodBuilder.cs
@@ -5,6 +5,10 @@ using System.Runtime.InteropServices;
 
 namespace CrazyPanda.UnityCore.PandaTasks
 {
+    /// <summary>
+    /// This is left only for compatibility with old code
+    /// </summary>
+    [Obsolete("Use PandaTaskMethodBuilder instead it")]
     public struct IPandaTaskMethodBuilder
     {
         private PandaTask< VoidResult > _task;
@@ -64,6 +68,7 @@ namespace CrazyPanda.UnityCore.PandaTasks
         }
     }
 
+    [Obsolete("Use PandaTaskMethodBuilder instead it")]
     public struct IPandaTaskMethodBuilder< TResult >
     {
         private PandaTask< TResult > _task;
@@ -87,14 +92,14 @@ namespace CrazyPanda.UnityCore.PandaTasks
             where TAwaiter : INotifyCompletion
             where TStateMachine : IAsyncStateMachine
         {
-            AwaitOnCompleted( ref awaiter, ref machine, ref _task );
+            PandaTaskMethodBuilder< TResult >.AwaitOnCompleted( ref awaiter, ref machine, ref _task );
         }
 
         public void AwaitUnsafeOnCompleted< TAwaiter, TStateMachine >( ref TAwaiter awaiter, ref TStateMachine machine )
             where TAwaiter : INotifyCompletion
             where TStateMachine : IAsyncStateMachine
         {
-            AwaitOnCompleted( ref awaiter, ref machine, ref _task );
+            PandaTaskMethodBuilder<TResult>.AwaitOnCompleted( ref awaiter, ref machine, ref _task );
         }
 
         public void SetResult( TResult value )
@@ -115,46 +120,6 @@ namespace CrazyPanda.UnityCore.PandaTasks
             }
 
             _task.Reject( ex );
-        }
-
-        internal static void AwaitOnCompleted< TAwaiter, TStateMachine >( ref TAwaiter awaiter, ref TStateMachine machine, ref PandaTask< TResult > taskField )
-            where TAwaiter : INotifyCompletion
-            where TStateMachine : IAsyncStateMachine
-        {
-            awaiter.OnCompleted( GetMoveNextAction( ref taskField, ref machine ) );
-        }
-
-        private static Action GetMoveNextAction< TStateMachine >( ref PandaTask< TResult > taskField, ref TStateMachine stateMachine )
-            where TStateMachine : IAsyncStateMachine
-        {
-            if( taskField is AsyncStateMachineTask< TStateMachine > stateMachineTask )
-            {
-                return stateMachineTask.MoveNextAction;
-            }
-
-            var ret = new AsyncStateMachineTask< TStateMachine >();
-
-            taskField = ret;
-            ret.StateMachine = stateMachine;
-
-            return ret.MoveNextAction;
-        }
-
-        [ DebuggerNonUserCode ]
-        private class AsyncStateMachineTask< TStateMachine > : PandaTask< TResult >
-            where TStateMachine : IAsyncStateMachine
-        {
-            private Action _moveNextAction;
-
-            public TStateMachine StateMachine;
-
-            public Action MoveNextAction => _moveNextAction ?? (_moveNextAction = MoveNextStateMachine);
-
-            // avoid using lambda cause it will generate another type
-            private void MoveNextStateMachine()
-            {
-                StateMachine.MoveNext();
-            }
         }
     }
 }
