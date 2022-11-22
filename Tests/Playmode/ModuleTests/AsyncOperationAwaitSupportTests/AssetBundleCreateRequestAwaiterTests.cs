207e3b9425a4c27910c755c8717c14e9fbdfad8a
Evgeniy Mineev <e.mineev@crazypanda.ru>
2022-11-22 15:25:35 +0400

    ignore tests at device

--- Tests/Playmode/ModuleTests/AsyncOperationAwaitSupportTests/AssetBundleCreateRequestAwaiterTests.cs
+++ Tests/Playmode/ModuleTests/AsyncOperationAwaitSupportTests/AssetBundleCreateRequestAwaiterTests.cs
@@ -5,6 +5,9 @@ using NUnit.Framework;
 
 namespace CrazyPanda.UnityCore.PandaTasks.Tests
 {
+#if !UNITY_EDITOR    
+    [Ignore("")]
+#endif    
     [ Category( "ModuleTests" ), Category( "LocalTests" ) ]
     public sealed class AssetBundleCreateRequestAwaiterTests
     {
